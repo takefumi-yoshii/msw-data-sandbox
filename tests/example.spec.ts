8ce42f7aff2575059669ae2a93368874263e42c8
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2022-01-18 23:09:54 +0900

    fix test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,13 +6,10 @@ async function takeScreenshot(page: Page, name: string) {
 test("edit post", async ({ page }) => {
   const expectText = "TEST";
   await page.goto("http://localhost:3000/posts");
-  await page.locator("text=Lorem ipsun").click();
-  await page.waitForNavigation();
+  await page.locator("text=Lorem ipsum").click();
   await page.locator("text=edit").click();
-  await page.waitForNavigation();
   await page.fill("[name=title]", expectText);
   await page.locator("button").click();
-  await page.waitForNavigation();
   const locator = page.locator("h2");
   await expect(locator).toHaveText(expectText);
   await takeScreenshot(page, "edit_post");
@@ -22,10 +19,8 @@ test("create post", async ({ page }) => {
   const expectText = "TEST";
   await page.goto("http://localhost:3000/posts");
   await page.locator("text=create new").click();
-  await page.waitForNavigation();
   await page.fill("[name=title]", expectText);
   await page.locator("button").click();
-  await page.waitForNavigation();
   const locator = page.locator("[data-testid=list] li:last-child");
   await expect(locator).toHaveText(expectText);
   await takeScreenshot(page, "create_post");
