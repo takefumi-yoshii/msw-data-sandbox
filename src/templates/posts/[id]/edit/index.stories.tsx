c99e8e3293302333f6b0e26dae7bb9dbeb3966a1
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2022-01-19 12:06:04 +0900

    fix(test): fix stories

--- src/templates/posts/[id]/edit/index.stories.tsx
+++ src/templates/posts/[id]/edit/index.stories.tsx
@@ -1,10 +1,9 @@
 import type { ComponentStoryObj } from "@storybook/react";
 import { Template } from "./";
-type Story = ComponentStoryObj<typeof Template>;
 
-export default {
-  component: Template,
-};
+type Story = ComponentStoryObj<typeof Template>;
+export default { component: Template };
+
 export const Default: Story = {
   args: { id: "0" },
 };
